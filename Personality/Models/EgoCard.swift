37eb193e47923d8acc5a34d17585b6214d0dbe9c
André Arns <andreluisarns@gmail.com>
2021-11-25 22:58:57 -0300

    Organize project

---
+++ Personality/Models/EgoCard.swift
@@ -0,0 +1,8 @@
+//
+//  EgoCard.swift
+//  Personality
+//
+//  Created by André Arns on 25/11/21.
+//
+
+import Foundation
---
+++ Personality/Scenes/Onboarding/Components/EgoCardView.swift
@@ -0,0 +1,20 @@
+//
+//  EgoCardView.swift
+//  Personality
+//
+//  Created by André Arns on 25/11/21.
+//
+
+import SwiftUI
+
+struct EgoCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EgoCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        EgoCardView()
+    }
+}
